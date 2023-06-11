0842c88aae0c2680dd5bab66949aab0ee15993a5
Vo Long <volonglqd@gmail.com>
2023-06-11 20:09:47 +0700

    Update code

--- src/views/ProductDetail.js
+++ src/views/ProductDetail.js
@@ -7,20 +7,39 @@ import {
   ScrollView,
   TouchableOpacity,
   ActivityIndicator,
+  Button,
 } from 'react-native';
-import {WooCommerceAPI} from '../utils/wooConfig';
 import Swiper from 'react-native-swiper';
 import Entypo from 'react-native-vector-icons/Entypo';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import RenderHtml from 'react-native-render-html';
+import {Picker} from '@react-native-picker/picker';
+import {ButtonGroup} from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import config from '../utils/config';
 
 const {width} = Dimensions.get('window');
+const WooCommerceAPI = axios.create({
+  baseURL: `${config.url}/wp-json/wc`,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  auth: {
+    username: `${config.consumer_key}`,
+    password: `${config.consumer_secret}`,
+  },
+});
 
 const ProductDetail = ({route, navigation}) => {
   const [product, setProduct] = useState(null);
   const [isExpanded, setIsExpanded] = useState(false);
   const [heigthRender, setHeightRender] = useState(70);
+  const [isLoading, setIsLoading] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const [selectedAttributes, setSelectedAttributes] = useState({});
   const {itemId} = route.params;
 
   useEffect(() => {
@@ -29,12 +48,68 @@ const ProductDetail = ({route, navigation}) => {
 
   const getProduct = async () => {
     try {
-      const response = await WooCommerceAPI.get(`/products/${itemId}`);
-      setProduct(response.data);
+      const response = await WooCommerceAPI.get(`/v3/products/${itemId}`);
       console.log('🚀 ~ getNewProducts ~ response.data:', response.data);
+      setProduct(response.data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+    if (heigthRender === 70) setHeightRender('auto');
+    else setHeightRender(70);
+  };
+
+  const addToCart = async () => {
+    const token = await AsyncStorage.getItem('accessToken');
+    setIsLoading(true);
+    try {
+      const productId = product?.id;
+      if (productId) {
+        const response = await WooCommerceAPI.post(
+          'store/v1/cart/add-item',
+          {
+            id: productId,
+            quantity,
+            variation: [
+              {
+                attribute: 'pa_color',
+                value: selectedAttributes['1'],
+              },
+              {
+                attribute: 'pa_size',
+                value: selectedAttributes['5'],
+              },
+            ],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        console.log('Add to cart success:', response.data);
+      } else {
+        console.log('Product ID is not available');
+      }
+    } catch (error) {
+      console.log('Add to cart error:', error.message);
+      console.log('Error response:', error.response);
+    }
+    setIsLoading(false);
+  };
+
+  const handleAttributeChange = (attributeId, value) => {
+    setSelectedAttributes(prevState => ({
+      ...prevState,
+      [attributeId]: value,
+    }));
+  };
+
+  const handleQuantityChange = value => {
+    setQuantity(value);
   };
 
   if (!product) {
@@ -50,12 +125,6 @@ const ProductDetail = ({route, navigation}) => {
       </View>
     );
   }
-
-  const handleExpand = () => {
-    setIsExpanded(!isExpanded);
-    if (heigthRender === 70) setHeightRender('auto');
-    else setHeightRender(70);
-  };
 
   return (
     <View
@@ -181,6 +250,7 @@ const ProductDetail = ({route, navigation}) => {
               justifyContent: 'space-between',
             }}>
             <TouchableOpacity
+              onPress={addToCart}
               activeOpacity={0.8}
               style={{
                 marginTop: 15,
@@ -200,9 +270,11 @@ const ProductDetail = ({route, navigation}) => {
                   width: 20,
                 }}
               />
-              <Text style={{color: '#fff', fontFamily: 'Poppins-Medium'}}>
-                Add to Cart
-              </Text>
+              {product?.id && (
+                <Text style={{color: '#fff', fontFamily: 'Poppins-Medium'}}>
+                  Add to Cart
+                </Text>
+              )}
             </TouchableOpacity>
             <TouchableOpacity
               activeOpacity={0.8}
@@ -215,7 +287,7 @@ const ProductDetail = ({route, navigation}) => {
                 paddingVertical: 12,
               }}>
               <Text style={{color: '#fff', fontFamily: 'Poppins-Medium'}}>
-                <AntDesign name="hearto" color="#fff" size={20} />
+                <AntDesign name="hearto" color="#fff" size={18} />
               </Text>
             </TouchableOpacity>
           </View>
@@ -258,7 +330,7 @@ const ProductDetail = ({route, navigation}) => {
                       marginTop: -8,
                       fontFamily: 'Poppins-Medium',
                     }}>
-                    View less
+                    View more
                   </Text>
                 </TouchableOpacity>
               ) : (
@@ -269,12 +341,49 @@ const ProductDetail = ({route, navigation}) => {
                       marginTop: -8,
                       fontFamily: 'Poppins-Medium',
                     }}>
-                    View more
+                    View less
                   </Text>
                 </TouchableOpacity>
               )}
             </View>
           )}
+        </View>
+        {product.attributes.map(
+          attribute =>
+            attribute.variation && (
+              <View key={attribute.id} style={{marginBottom: 10}}>
+                <Text style={{fontWeight: 'bold'}}>{attribute.name}</Text>
+                <ButtonGroup
+                  buttons={attribute.options}
+                  selectedIndex={attribute.options.indexOf(
+                    selectedAttributes[attribute.id],
+                  )}
+                  onPress={selectedIndex =>
+                    handleAttributeChange(
+                      attribute.id,
+                      attribute.options[selectedIndex],
+                    )
+                  }
+                />
+              </View>
+            ),
+        )}
+        <Text>{JSON.stringify(selectedAttributes)}</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 10,
+          }}>
+          <Button
+            title="-"
+            onPress={() => handleQuantityChange(Math.max(1, quantity - 1))}
+          />
+          <Text style={{paddingHorizontal: 10}}>{quantity}</Text>
+          <Button
+            title="+"
+            onPress={() => handleQuantityChange(quantity + 1)}
+          />
         </View>
         <View style={{marginBottom: 80}} />
       </ScrollView>
